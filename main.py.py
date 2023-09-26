1355e11469afd299c757cfc815ff9cb60483c6f1
Faizhazu <136839470+Faizhazu@users.noreply.github.com>
2023-09-26 11:04:37 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -17,18 +17,22 @@ def load_image(image_path):
 
 # Create the pipeline
 pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
-    MODEL_ID, torch_dtype=torch.float16, safety_checker=None
+    MODEL_ID,
+    torch_dtype=torch.float16,  # Use torch.float16 for reduced memory usage
+    safety_checker=None  # Set the safety checker to None if not needed
 )
-pipe.to("cuda")
+pipe.to("cuda")  # Move the pipeline to the GPU for faster processing
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
 # Load the input image
 image = load_image(IMAGE_PATH)
 
 # Generate the cartoonized image
-images = pipe(PROMPT, image=image, num_inference_steps=10, image_guidance_scale=1).images
+num_inference_steps = 10
+image_guidance_scale = 1
+images = pipe(PROMPT, image=image, num_inference_steps=num_inference_steps, image_guidance_scale=image_guidance_scale).images
 
 # Display the cartoonized image
 plt.imshow(images[0])
+plt.axis('off')  # Turn off axis labels
 plt.show()
-
